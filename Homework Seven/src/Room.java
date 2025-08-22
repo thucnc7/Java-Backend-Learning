3e5430b35a9f9ecd14e427dff54bedc51da579fe
LeSyThuc <nexythuc2602@gmail.com>
2025-08-22 10:28:36 +0700

    Upload UML Class Design for Homework 07

--- Homework Seven/src/Room.java
+++ Homework Seven/src/Room.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Array;
 
 public class Room {
     public String RoomId;
-    public static String[] RoomTypeList = new String[]{"President", "Luxary", "Normal", "Economic"};
+    public static String[] RoomTypeList = new String[]{"President", "Luxary", "Standard", "Econo"};
     public static Integer[] RoomPriceList = new Integer[]{10000,1000,100,10};
     public int RoomType;
     public Room(String RoomId , int RoomType){
